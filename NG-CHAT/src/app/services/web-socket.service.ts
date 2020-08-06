e835fdb083d3b50668489f69ee619ee8568f2144
Carlos Revilla <clro26.k@gmail.com>
2020-08-06 23:55:56 +0200

    declared services http

--- NG-CHAT/src/app/services/web-socket.service.ts
+++ NG-CHAT/src/app/services/web-socket.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import * as io from 'socket.io-client';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketService {
 
+  socket: any;
+  server = "http://localhost:3000"
+
   constructor() { }
+
 }
