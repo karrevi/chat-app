75e5af3d283ee440277dbd6d645e28598a4613de
Carlos Revilla <clro26.k@gmail.com>
2020-08-07 00:07:04 +0200

    established method to receive/listen to server message

--- NG-CHAT/src/app/services/web-socket.service.ts
+++ NG-CHAT/src/app/services/web-socket.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
-import { Observable } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,15 @@ export class WebSocketService {
   socket: any;
   server = "http://localhost:3000"
 
-  constructor() { }
-
+  constructor() {
+    this.socket = io(this.server)
+  }
+  listen(eventName: String) {
+    return new Observable((Subscriber) => {
+      this.socket.on(eventName, (data) => {
+        Subscriber.next(data);
+      })
+    })
+  }
+  
 }
