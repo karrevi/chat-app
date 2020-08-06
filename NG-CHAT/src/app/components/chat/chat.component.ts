8a1d2164189de86556724567892ec0c84d5ac65f
Carlos Revilla <clro26.k@gmail.com>
2020-08-07 00:52:51 +0200

    declared function, variable, event

--- NG-CHAT/src/app/components/chat/chat.component.ts
+++ NG-CHAT/src/app/components/chat/chat.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { WebSocketService } from '../../services/web-socket.service';
+
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  userChat = {
+    user: '',
+    text: ''
+  }
+
+  myMessages;
+  eventName = "send-message";
+
+  constructor(
+    private activated: ActivatedRoute, private webService: WebSocketService
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activated.snapshot.params.id;
+    this.userChat.user = id;
+
+    this.webService.listen('text-event').subscribe((data) => {
+      this.myMessages = data;
+    })
+  }
+
+  myMessage() {
+    this.webService.emit(this.eventName, this.userChat);
+    this.userChat.text = ''
   }
 
 }
