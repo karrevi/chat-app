978b0f7b4120804627b048171a037b777b77f06f
Carlos Revilla <clro26.k@gmail.com>
2020-08-06 23:37:19 +0200

    created routes

--- NG-CHAT/src/app/app-routing.module.ts
+++ NG-CHAT/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChatComponent } from './components/chat/chat.component';
+import { HomeComponent } from './components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'chat/:id',
+    component: ChatComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
